e8c7c88dff49b6ec3df8ab02e14cfd078be1c4c2
Hoàn Linh13 <hoanlinhpd@gmail.com>
2025-01-13 21:42:35 +0700

    change J1.S.P0010

--- J1.S.P0010/src/main/java/com/mycompany/j1/s/p0010/model/DataOperation.java
+++ J1.S.P0010/src/main/java/com/mycompany/j1/s/p0010/model/DataOperation.java
@@ -1,8 +1,6 @@
 package com.mycompany.j1.s.p0010.model;
 
 public interface DataOperation {
-    void setArray(int[] array);
-    int[] getArray();
     int search(int x);
     void displayArray();
 }
