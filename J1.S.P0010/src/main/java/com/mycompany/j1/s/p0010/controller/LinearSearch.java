f05502c212c2b9051492744ae6d1b274e1af10d2
Hoàn Linh13 <hoanlinhpd@gmail.com>
2025-01-13 22:19:01 +0700

    update J1.S.P0010

--- J1.S.P0010/src/main/java/com/mycompany/j1/s/p0010/controller/LinearSearch.java
+++ J1.S.P0010/src/main/java/com/mycompany/j1/s/p0010/controller/LinearSearch.java
@@ -26,9 +26,8 @@ public class LinearSearch implements DataOperation {
 
     private int[] generateRandomArray() {
         Random rand = new Random();
-        this.array = new int[size];
         for (int i = 0; i < size; i++) {
-            array[i] = rand.nextInt(100) - 50; //  Giới hạn từ -50 đến 50
+            this.array[i] = rand.nextInt(100) - 50; //  Giới hạn từ -50 đến 50
         }
 
         return array;
